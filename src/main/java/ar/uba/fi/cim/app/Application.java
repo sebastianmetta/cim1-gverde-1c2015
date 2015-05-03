6e68246544c42310fb7742fb80b22bfcd12aa1f0
sebastianmetta <sebastianmetta@gmail.com>
2015-05-02 22:04:06 -0300

    Primer commit, pom básico y clase inicial.

---
+++ src/main/java/ar/uba/fi/cim/app/Application.java
@@ -0,0 +1,10 @@
+package ar.uba.fi.cim.app;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/main/java/ar/uba/fi/cim/files/FilesProcessor.java
@@ -0,0 +1,12 @@
+package ar.uba.fi.cim.files;
+
+/**
+ * Responsable de leer las imágenes del directorio correspondiente y notificar
+ * al procesador para que las analice.
+ * 
+ * @author Sebastian
+ *
+ */
+public class FilesProcessor {
+
+}
