2055b073f5e70b2b27518c1580e11296e496b104
mlucero88 <luceromartinandres89630@gmail.com>
2015-05-30 16:56:42 -0300

    Generador de csv. Arg1: carpeta con fotos de huevos chicos Arg2: carpeta con fotos de huevos grandes Arg3: filename de csv

--- src/main/java/ar/uba/fi/cim/app/Application.java
+++ src/main/java/ar/uba/fi/cim/app/Application.java
@@ -1,17 +1,107 @@
 package ar.uba.fi.cim.app;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
 
-import ar.uba.fi.cim.files.FilesProcessor;
+import javax.imageio.ImageIO;
+
+import ar.uba.fi.cim.filter.Histograma;
 
 public class Application {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext appContext = new ClassPathXmlApplicationContext("/application-context.xml");
-		FilesProcessor process = (FilesProcessor) appContext.getBean("filesProccessorBean");
-		process.startProcessing();
-		appContext.close();
+	private static final int NUM_RANGOS = 10;
+	private static FileWriter fileWriter = null;
+
+	private static final String COMMA_DELIMITER = ",";
+	private static final String NEW_LINE_SEPARATOR = "\n";
+
+	private static void procesarImagen(File file, boolean esChico) {
+		BufferedImage image;
+		try {
+			image = ImageIO.read(file);
+
+			// BinarizationFilter binFilter = new BinarizationFilter(image);
+			// binFilter.setThreshold(50);
+			// binFilter.doBinirization();
+			// Histograma histograma = new Histograma(binFilter.getImg());
+
+			Histograma histograma = new Histograma(image);
+
+			int[][] arrayHistograma = histograma.getHistograma();
+
+			// Armo contadores de rango de cantidad de pixeles
+			double[] rangos = new double[NUM_RANGOS];
+			double intervalo = (double) 256 / NUM_RANGOS;
+			double cotaRango = intervalo;
+			int cantPixeles = 0, rango = 0;
+
+			for (int j = 0; j < 256; j++) {
+				if (j > cotaRango) {
+					cotaRango += intervalo;
+					rango++;
+				}
+				cantPixeles += arrayHistograma[0][j];
+				rangos[rango] += arrayHistograma[0][j];
+			}
+
+			// Normalizacion de contadores
+			for (int i = 0; i < NUM_RANGOS; i++) {
+				rangos[i] /= cantPixeles;
+			}
+
+			// Generacion csv
+			for (double valor : rangos) {
+				fileWriter.append(String.format(Locale.US, "%.10f", valor));
+				fileWriter.append(COMMA_DELIMITER);
+			}
+
+			if (esChico) {
+				fileWriter.append(String.valueOf(0));
+			}
+			else {
+				fileWriter.append(String.valueOf(1));
+			}
+
+			fileWriter.append(NEW_LINE_SEPARATOR);
+		}
+		catch (IOException e) {
+			System.out.println("Image " + file.getName() + " failed");
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+
+		final File folderChico = new File(args[0]);
+		final File folderGrande = new File(args[1]);
+		try {
+			fileWriter = new FileWriter(args[2]);
+		}
+		catch (IOException e) {
+			System.out.println("Error in CsvFileWriter !!!");
+			e.printStackTrace();
+			return;
+		}
+
+		for (final File file : folderChico.listFiles()) {
+			procesarImagen(file, true);
+		}
+
+		for (final File file : folderGrande.listFiles()) {
+			procesarImagen(file, false);
+		}
+
+		try {
+			fileWriter.flush();
+			fileWriter.close();
+		}
+		catch (IOException e) {
+			System.out.println("Error while flushing/closing fileWriter !!!");
+			e.printStackTrace();
+		}
+		System.out.println("TERMINO");
+	}
 }
