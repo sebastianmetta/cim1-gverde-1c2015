08fc35e4ab0d5b0f0aa6e79e0a41d33ac7df379c
martin <luceromartinandres89630@gmail.com>
2015-06-12 16:42:50 -0300

    Generacion csv con binarizacion, falta sobel

--- src/main/java/ar/uba/fi/cim/app/Application.java
+++ src/main/java/ar/uba/fi/cim/app/Application.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 
 import javax.imageio.ImageIO;
 
+import ar.uba.fi.cim.filter.BinarizationFilter;
 import ar.uba.fi.cim.filter.Histograma;
 
 public class Application {
@@ -22,52 +23,35 @@ public class Application {
 		BufferedImage image;
 		try {
 			image = ImageIO.read(file);
-
-			// BinarizationFilter binFilter = new BinarizationFilter(image);
-			// binFilter.setThreshold(50);
-			// binFilter.doBinirization();
-			// Histograma histograma = new Histograma(binFilter.getImg());
-
-			Histograma histograma = new Histograma(image);
+			BinarizationFilter binFilter = new BinarizationFilter(image);
+			binFilter.setThreshold(50);
+			binFilter.doBinirization();
+			Histograma histograma = new Histograma(binFilter.getImg());
 
 			int[][] arrayHistograma = histograma.getHistograma();
 
-			// Armo contadores de rango de cantidad de pixeles
-			double[] rangos = new double[NUM_RANGOS];
-			double intervalo = (double) 256 / NUM_RANGOS;
-			double cotaRango = intervalo;
-			int cantPixeles = 0, rango = 0;
-
-			for (int j = 0; j < 256; j++) {
-				if (j > cotaRango) {
-					cotaRango += intervalo;
-					rango++;
-				}
-				cantPixeles += arrayHistograma[0][j];
-				rangos[rango] += arrayHistograma[0][j];
-			}
+			double cantPixBlancoBin = arrayHistograma[0][255];
+			double cantPixNegroBin = arrayHistograma[0][0];
+			int cantPix = (int) (cantPixBlancoBin + cantPixNegroBin);
 
 			// Normalizacion de contadores
-			for (int i = 0; i < NUM_RANGOS; i++) {
-				rangos[i] /= cantPixeles;
-			}
+			cantPixBlancoBin /= cantPix;
+			cantPixNegroBin /= cantPix;
 
 			// Generacion csv
-			for (double valor : rangos) {
-				fileWriter.append(String.format(Locale.US, "%.10f", valor));
-				fileWriter.append(COMMA_DELIMITER);
-			}
+			fileWriter.append(String.format(Locale.US, "%.10f", cantPixBlancoBin));
+			fileWriter.append(COMMA_DELIMITER);
+			fileWriter.append(String.format(Locale.US, "%.10f", cantPixNegroBin));
+			fileWriter.append(COMMA_DELIMITER);
 
 			if (esChico) {
 				fileWriter.append(String.valueOf(0));
-			}
-			else {
+			} else {
 				fileWriter.append(String.valueOf(1));
 			}
 
 			fileWriter.append(NEW_LINE_SEPARATOR);
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Image " + file.getName() + " failed");
 			e.printStackTrace();
 		}
@@ -79,8 +63,7 @@ public class Application {
 		final File folderGrande = new File(args[1]);
 		try {
 			fileWriter = new FileWriter(args[2]);
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error in CsvFileWriter !!!");
 			e.printStackTrace();
 			return;
@@ -97,8 +80,7 @@ public class Application {
 		try {
 			fileWriter.flush();
 			fileWriter.close();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error while flushing/closing fileWriter !!!");
 			e.printStackTrace();
 		}
